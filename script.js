2df4d1f2ff9ef678e11ccb03d8c912abb6c2b817
Muremwaro <muremwaro647@gmail.com>
2026-05-06 10:47:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+/*let age, yob, currentYear;
+yob=prompt("your year of birth");
+currentYear=prompt("current year");
+age = currentYear - yob; 
+alert("your age is " + age);
+document.getElementsById("contactbtn").addEventListener("click", function(){
+   window.location.href="portfolio.html";});
+   const massMark = 78;
+   const heightMark = 1.69;
+   const massJohn = 92;
+   const heightJohn =1.95;
+   const BMIMark = massMark/(heightMark * heightMark);
+    const BMIJohn = massJohn/(heightJohn * heightJohn);
+    console.log( BMIMark, BMIJohn);*/
+    document.getElementById("contactbtn").addEventListener("click", function(){
+        window.location.href="about.html";
+    });
+    
